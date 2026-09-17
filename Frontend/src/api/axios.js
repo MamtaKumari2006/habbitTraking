8adf97ac0d4bd7fe167254c020f1610c72b4abb8
MamtaKumari2006 <mamtasony264@gmail.com>
2026-09-17 15:55:28 +0530

    cors error

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     
-    baseURL: "https://APNA-RENDER-BACKEND-URL.onrender.com/api",
+    baseURL: "https://habbittraking.onrender.com//api",
 });
 
 
