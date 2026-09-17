2c0793804425d7e575e2ab5f170dca7963fc26f7
MamtaKumari2006 <mamtasony264@gmail.com>
2026-09-17 15:19:31 +0530

    cors error fix

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-    // Yahan apna Render wala link dalo aur end me /api lagao ✅
+    
     baseURL: "https://APNA-RENDER-BACKEND-URL.onrender.com/api",
 });
 
-// Automatic token attach karne ke liye (taaki baar baar header na likhna pade)
+
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
